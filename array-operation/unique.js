a30c85e57dc9aedd7ab34fe3230bae1b7abdeb0a
张红豆 <zhanghongdou@enn.cn>
2020-11-11 00:35:14 +0800

    commit

---
+++ array-operation/unique.js
@@ -0,0 +1,8 @@
+const arr = [1, 1, '1', 15, true, true, false, false, 'true', 'a', {}, {}, function() {return [1,2]}]
+
+// 数组去重
+// 方法一： Array.from
+// const res1 = Array.from(new Set(arr))
+// console.log(res1)
+
+// 
---
+++ modularity/01/js/index.js
@@ -0,0 +1,24 @@
+function throttle(fn, delay) {
+  var timer = null,
+      begin = new Date().getTime();
+  
+  return function() {
+    const curTime = new Date().getTime(),
+          _self = this,
+          args = arguments;
+    
+    clearTimeout(timer);
+    if (curTime - begin > delay) {
+      fn.apply(_self, args)
+      begin = curTime;
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(_self, args)
+      }, delay)
+    }
+  }
+}
+
+export default {
+  throttle
+}

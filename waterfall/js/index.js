8038664e50ec6c8b376dd8533a32befaafde8a1b
张红豆 <zhanghongdou@enn.cn>
2020-11-12 00:44:48 +0800

    完成瀑布流展示图片的功能

--- waterfall/js/index.js
+++ waterfall/js/index.js
@@ -21,9 +21,20 @@
         } else {
           item.style.left = (232 + 10) * i + 'px';
         }
+      } else {
+        minIndex = getMinIndexInArr(_arr);
+        item.style.left = oItems[minIndex].offsetLeft + 'px';
+        item.style.top = (_arr[minIndex] + 10) + 'px';
+        // 最小高度一行添加一张图片以后，需要更新该行的高度
+        _arr[minIndex] += item.offsetHeight + 10;
       }
     }
+    console.log()
     console.log(_arr)
+  }
+
+  const getMinIndexInArr = (arr) => {
+    return [].indexOf.call(arr, Math.min.apply(null, arr))
   }
 
   window.onload = () => {

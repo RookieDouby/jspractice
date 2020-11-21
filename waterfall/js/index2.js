f46bdd1470b31aa6c54694209b909fc888432ebe
张红豆 <zhanghongdou@enn.cn>
2020-11-21 23:40:03 +0800

    瀑布流图片

--- waterfall/js/index2.js
+++ waterfall/js/index2.js
@@ -1,12 +1,35 @@
 ;(function(doc) {
-  var WaterFall = function(wrapper, options){
-    this.oWrapper = doc.getElementsByClassName(wrapper)[0]
-    this.column = options.column;
-    this.gap = options.gap;
+  var WaterFall = function(wrapper, opt) {
+    this.oWrapper = document.getElementsByClassName(wrapper)[0];
+    this.column = opt.column;
+    this.gap = opt.gap;
+    this.itemWidth = (this.oWrapper.offsetWidth - (this.column - 1) * this.gap) / this.column;
+    this.pageNum = 0;
+    this.pageSize = 0;
+    this.heightArr = []; // 前5个高度
+    
 
-    console.log(this.oWrapper, this.column, this.gap)
+    console.log(this.oWrapper, this.column, this.gap);
   }
 
+  WaterFall.prototype = {
+    init: function() {
+      this.bindEvent()
+    },
+    bindEvent: function() {
+
+    },
+    scrollToBtn: function() {
+
+    },
+    getImgData: function() {
+      // http://shibe.online/api/shibes?count=20 接口获取20张图片的接口 
+    },
+    renderList: function() {
+      
+    },
+
+  }
 
   window.WaterFall = WaterFall;
 })(document)
